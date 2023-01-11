11ba7c2c7f3078cbd5088132aba39e60721b109e
Yohan Silva <yohanps.dev@gmail.com>
2023-01-10 21:06:48 -0300

    codeWar: resolve 'StringEndsWith' problem

---
+++ src/string/kyu7/StringEndsWith.java
@@ -0,0 +1,17 @@
+package string.kyu7;
+
+/**
+DESCRIPTION:
+        Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+        Examples:
+        solution('abc', 'bc') // returns true
+        solution('abc', 'd') // returns false
+**/
+
+public class StringEndsWith {
+
+    public static boolean stringEndsWith(String firstArgument, String secondArgument) {
+        return firstArgument.endsWith(secondArgument);
+    }
+}
