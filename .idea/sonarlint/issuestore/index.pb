6f9339d2d08b221a3d9690d223d69104e9f61e8e
Yohan Silva <yohanps.dev@gmail.com>
2023-04-16 13:44:32 -0300

    CodeWars: send EmotionalSorting challenge

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+;
+CodeWar.iml,4\0\40a6cf158cf8c5461e501a579b10e3a12c515e3e
+U
+%src/desafios/kyu7/StringEndsWith.java,1\7\17aca4b8493c8619636037cb5d8cbf807a4f8233
+T
+$src/desafios/kyu7/SumOddNumbers.java,6\b\6b8d23beb6c427289c44a035517ebed68cd6099a
+S
+#src/desafios/kyu8/VowelRemover.java,8\5\85dd94039ac785fd0703f7032facfc9244816536
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
---
+++ src/desafios/kyu6/EmotionalSorting.java
@@ -0,0 +1,56 @@
+package desafios.kyu6;
+
+import java.util.*;
+
+/**
+ *
+ * Emotional Sort ( ︶︿︶)
+ * You'll have a function called "sortEmotions" that will return an array of emotions sorted. It has two parameters, the first parameter called "arr" will expect an array of emotions where an emotion will be one of the following:
+ *
+ * :D -> Super Happy
+ * :) -> Happy
+ * :| -> Normal
+ * :( -> Sad
+ * T_T -> Super Sad
+ * Example of the array:[ 'T_T', ':D', ':|', ':)', ':(' ]
+ *
+ * And the second parameter is called "order", if this parameter is true then the order of the emotions will be descending (from Super Happy to Super Sad) if it's false then it will be ascending (from Super Sad to Super Happy)
+ *
+ * Example if order is true with the above array: [ ':D', ':)', ':|', ':(', 'T_T' ]
+ *
+ * Super Happy -> Happy -> Normal -> Sad -> Super Sad
+ * If order is false: [ 'T_T', ':(', ':|', ':)', ':D' ]
+ *
+ * Super Sad -> Sad -> Normal -> Happy -> Super Happy
+ * Example:
+ *
+ * arr = [':D', ':|', ':)', ':(', ':D']
+ * sortEmotions(arr, true) // [ ':D', ':D', ':)', ':|', ':(' ]
+ * sortEmotions(arr, false) // [ ':(', ':|', ':)', ':D', ':D' ]
+ * More in test cases!
+ *
+ * Notes:
+ *
+ * The array could be empty, in that case return the same empty array ¯\_( ツ )_/¯
+ * All emotions will be valid
+ * Enjoy! (づ｡◕‿‿◕｡)づ
+ */
+
+
+public class EmotionalSorting {
+    public static String[] sortEmotions(boolean order,String[] emotions) {
+        Map<String, Integer> classifyEmotion = new HashMap<>();
+        classifyEmotion.put("T_T", 1);
+        classifyEmotion.put(":(", 2);
+        classifyEmotion.put(":|", 3);
+        classifyEmotion.put(":)", 4);
+        classifyEmotion.put(":D", 5);
+
+        Arrays.sort(emotions, order
+                ? Comparator.comparingInt(classifyEmotion::get)
+                : Comparator.comparingInt(classifyEmotion::get).reversed()
+        );
+
+        return emotions;
+    }
+}
