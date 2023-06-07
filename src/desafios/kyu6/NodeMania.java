97ca52b64c5f8302939b1813cd972533bf9e792e
Yohan Silva <yohanps.dev@gmail.com>
2023-06-06 22:39:27 -0300

    CodeWars: send NodeMania challenge

--- src/desafios/kyu6/NodeMania.java
+++ src/desafios/kyu6/NodeMania.java
@@ -1,54 +1,46 @@
 package desafios.kyu6;
 
-
 /*
-This kata is about singly linked list. A linked list is an ordered set of data elements, each containing a link to its
+
+This kata is about singly-linked lists. A linked list is an ordered set of data elements, each containing a link to its
 successor (and sometimes its predecessor, known as a double linked list). You are you to implement an algorithm to find
 the kth to last element.
 
-For example given a linked list of:
+        k will be an integer greater than or equal to 1.
 
-a -> b -> c -> d
+        Example
+        For example given the following linked list: a -> b -> c -> d
 
-if k is the number one then d should be returned
-if k is the number two then c should be returned
-if k is the number three then b should be returned
-if k is the number four then a should be returned
-if k exceeds the size of the list then null returned
+        if k = 1 then d should be returned
+        if k = 2 then c should be returned
+        if k = 3 then b should be returned
+        if k = 4 then a should be returned
+        if k exceeds the length of the list then None(Python) or null(Java, JavaScript) should be returned
+        Each item in the linked list is a Node containing two fields:
 
-Special Note --> Node classes contain two fields; data and next. So if you want to assign an int the node data from
-Node node do int example = node.data
+        data - the value of the node
+        next - pointing to the next node in the list, or to a null reference (null/NULL/None, depending on your language)
+         for the last Node.
+        An empty list is represented as a null reference.
  */
 
- class NodeMania {
-     public static Integer searchKFromEnd(Node linkedList, int k){
-        return null;
+
+public class NodeMania {
+
+// Abaixo está a resposta, mas o CodeWar utiliza uma classe interna chamada Node para o exercicio. sendo assim, não
+// tenho escolha a não ser comentar;
+
+    /*
+        public static Integer searchKFromEnd(Node linkedList, int k){
+        List<Integer> list = new ArrayList<Integer>();
+
+        while (linkedList != null) {
+            list.add(linkedList.data);
+            linkedList = linkedList.next;
+        }
+
+        int len = list.size();
+        return len < k ? null : list.get(len - k);
     }
-}
-class Node<T> {
-
-    private T element;
-    private Node<T> next;
-    private Node<T> previus;
-
-    public Node(T element, Node<T> next, Node<T> previus) {
-        this.element = element;
-        this.next = next;
-        this.previus = previus;
-    }
-
-}
-
-class LinkedList<> {
-    private Node<T> primeiroNo;
-    private Node<T> ultimoNo;
-    private int tamanho;
-
-    public Node(int data) {
-        this.data = data;
-    }
-
-    public void build() {
-
-    }
+     */
 }
