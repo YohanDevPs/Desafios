577a663ed5182aec9b1a79c4ed73e821112bce34
Yohan Silva <yohanps.dev@gmail.com>
2023-06-19 22:36:43 -0300

    CodeWars: create Columnize class

--- src/desafios/Main.java
+++ src/desafios/Main.java
@@ -1,32 +1,12 @@
 package desafios;
 
-import desafios.kyu7.Rotations;
-import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.testng.AssertJUnit.assertEquals;
+import desafios.kyu6.Columnize;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-	}
-	private static void testing(Boolean actual, Boolean expected) {
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void test() {
-		System.out.println("Fixed Tests containAllRots");
-		testing(Rotations.containAllRots("", Arrays.asList()), true);
-		List<String> a = Arrays.asList("bsjq", "qbsj");
-		testing(Rotations.containAllRots("", a), true);
-		a = Arrays.asList("bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs");
-		testing(Rotations.containAllRots("bsjq", a), true);
-		a = Arrays.asList("TzYxlgfnhf", "yqVAuoLjMLy", "BhRXjYA", "YABhRXj", "hRXjYAB", "jYABhRX", "XjYABhR", "ABhRXjY");
-		testing(Rotations.containAllRots("XjYABhR", a), false);
+		String[] input = { "", "12", "123", "", "12345", "" };
+		System.out.println(Columnize.columnize(input,2));
 	}
 }
 
--- src/desafios/kyu6/Columnize.java
+++ src/desafios/kyu6/Columnize.java
@@ -1,4 +1,65 @@
 package desafios.kyu6;
 
+/*
+You are given an array of strings that need to be spread among N columns. Each column's width should be the same as the
+length of the longest string inside it.
+
+Separate columns with " | ", and lines with "\n"; content should be left-justified.
+
+{"1", "12", "123", "1234", "12345", "123456"} should become:
+
+1
+12
+123
+1234
+12345
+123456
+for 1 column,
+
+1     | 12
+123   | 1234
+12345 | 123456
+for 2 columns,
+
+1     | 12     | 123 | 1234
+12345 | 123456
+
+*/
+
 public class Columnize {
+    public static String columnize(String[] input, int numberOfColumns) {
+
+        String mostLargeString = "";
+        for (String st: input) {
+            if(mostLargeString.length() < st.length()) {
+                mostLargeString = st;
+            }
+        }
+
+        String answer = "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < mostLargeString.length(); i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                answer += input[i] + getEmptySpace(input[i].length(), mostLargeString.length()) + " | ";
+            }
+            answer = answer.substring(0, answer.length() - 3);
+            stringBuilder.append(answer + "\n");
+        }
+
+
+        return stringBuilder.toString();
+    }
+
+    public static String getEmptySpace(int txt, int number) {
+        int emptySpacesLeft = txt - number;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < emptySpacesLeft; i++) {
+            sb.append(" ");
+        }
+
+        return sb.toString();
+    }
 }
+
